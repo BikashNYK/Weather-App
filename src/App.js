c8d8cfa2c2063d6076a37810a01277c3e6e7dd61
Bikash Kumar Nayak <fluxtrn@gmail.com>
2022-12-12 00:23:40 +0530

    weather api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { useState } from 'react';
+// import { Button } from 'react-bootstrap';
 import './App.css';
-
+import axios from 'axios'
 function App() {
+
+  const[temp,setTemp]=useState(0);
+  const[humidity,setHumidity]=useState(0);
+  const[cityText,setCityText]=useState("")
+
+  async function fetchWeather(){
+
+    console.log('fetch weather of ' + cityText)
+    // https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8ceb190ec7d2e7450b4d24a84d9e057e
+// https://api.openweathermap.org/data/2.5/weather?q=asansol&appid=477b0577b454816f45f8516126ea4bb8
+    try {
+      let response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${cityText}&appid=477b0577b454816f45f8516126ea4bb8`)
+      // console.log(response)
+      // console.log(response.data.main.humidity);
+      // console.log(response.data.main.temp);
+      setTemp(response.data.main.temp)
+      setHumidity(response.data.main.humidity)
+    } catch (error) {
+      console.log(error);
+    }
+
+
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <h1>Weather App</h1>
+        <input value={cityText} onChange={(e)=>{setCityText(e.target.value)}} className='form-control' type="text" placeholder='Enter City'/>
+        <button onClick={fetchWeather} style={{ marginTop: "10px" }} className='btn btn-outline-primary'>Search Weather</button>
+        {/* This Button for react bootstap we have to import react bootstrap from button */}
+        {/* <Button>Search Weather</Button> */}
+      </div>
+
+      <div className='box'>
+        <h3>Temp : {temp} F</h3>
+        <h3>Humidity : {humidity}%</h3>
+      </div>
     </div>
   );
 }
